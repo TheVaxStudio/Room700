bd44daddab5478071750a0b7154321cc04a42ae0
Marcus Vinicius <marcus_viniciusreis@hotmail.com>
2025-06-07 10:38:37 -0300

    update: refactored Game.py to lowercase functions and variables

--- Scripts/Game.py
+++ Scripts/Game.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from Settings import *
+from Settings import settings as stg
 from Tilemap import Tilemap
 from Camera import Camera
 from Player import Player
@@ -9,29 +9,37 @@ from Light import Light
 class Game:
     def __init__(self):
         pygame.init()
-        self.Screen = pygame.display.set_mode((ScreenWidth, ScreenHeight), pygame.RESIZABLE)
-        self.Clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((stg['screen_width'], stg['screen_height']), pygame.RESIZABLE)
+        self.clock = pygame.time.Clock()
         self.tilemap = Tilemap()
         self.light = Light()
         self.player = Player(self.tilemap)
         self.camera = Camera(128, 128)
-        pygame.display.set_caption(Title)
-        self.Render()
+        pygame.display.set_caption(stg['title']) 
+        self.loop()    
 
-    def Render(self):
-        self.Running = True
-        self.frames = 0
-        while self.Running:
-            self.Clock.tick(60)
-            self.frames += 1
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.Running = False
-                    pygame.quit()
-                    sys.exit()
-            self.Screen.fill(Color)
-            self.tilemap.draw(self.Screen, self.camera)
-            self.light.draw(self.Screen)
-            self.player.draw(self.Screen, self.camera)
-            self.player.update(self.frames)
-            pygame.display.flip()
+    def loop(self):
+        self.running = True
+        self.elapsed_frames = 0
+        while self.running:
+            self.update()
+            self.render()                            
+
+    def update(self):
+        self.clock.tick(stg['fps'])
+        self.elapsed_frames += 1
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                pygame.quit()
+                sys.exit()                    
+        self.player.update(self.elapsed_frames)
+
+    def render(self):        
+        self.screen.fill(stg['light']['color'])
+        self.tilemap.draw(self.screen, self.camera)
+        self.light.draw(self.screen)
+        self.player.draw(self.screen, self.camera)
+        pygame.display.flip()
+        
+            
